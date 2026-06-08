257b8639c04a3f188713e1b5e46015315069146c
himanshu8655 <himanshu8655@gmail.com>
2026-06-07 22:27:03 -0400

    Add: string-encode-and-decode - submission-11

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-11.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder builder = new StringBuilder();
+        for(String str : strs){
+            builder.append(str.length()).append("&").append(str);
+        }
+        System.out.println("1"+builder.toString());
+    return builder.toString();
+    }
+
+    public List<String> decode(String s) {
+        int ptr = 0;
+        List<String> list = new ArrayList<>();
+        while(ptr<s.length()){
+            int delimeterIndex = s.indexOf("&",ptr);
+            System.out.println(s.substring(ptr, delimeterIndex));
+            int n = Integer.parseInt(s.substring(ptr, delimeterIndex));
+            list.add(s.substring(delimeterIndex+1, delimeterIndex+1+n));
+            ptr=delimeterIndex+1+n;
+        }
+
+        return list;
+    }
+}
