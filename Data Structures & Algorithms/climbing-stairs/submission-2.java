28307e980b972c409cd745237d2f4a50da690898
himanshu8655 <himanshu8655@gmail.com>
2026-06-15 23:40:48 -0400

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n];
+        if(n<=3)
+            return n;
+        dp[2] = 2;
+        dp[3] = 3;
+        for(int i = 4; i<n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n-1];
+    }
+}
