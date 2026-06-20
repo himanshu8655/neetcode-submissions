ce53775ba2f9e1a9218c8b7149f026da46e722fc
himanshu8655 <himanshu8655@gmail.com>
2026-06-20 17:40:48 -0400

    Add: longest-substring-without-duplicates - submission-10

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-10.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int[] freq = new int[128];
+        int result = 0;
+        int n = s.length();
+        int left = 0;
+        for(int i = 0; i<n; i++){
+            char c = s.charAt(i);
+            freq[c]++;
+            while(freq[c]<=1){
+                char lChar = s.charAt(left);
+                freq[lChar]--;
+                left++;
+            }
+
+            result = Math.max(result, i - left + 1);
+        }
+        return result;
+    }
+}
