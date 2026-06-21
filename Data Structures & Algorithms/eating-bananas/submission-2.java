168ce5c9d5348f832ef2fdff89abe7e0d21a039d
himanshu8655 <himanshu8655@gmail.com>
2026-06-21 19:19:54 -0400

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int right = Arrays.stream(piles).max().orElse(0);
+        int left = 0;
+        int result = h;
+        while(left<=right){
+            int mid = left + (right - left)/2;
+
+            if(canEat(piles, mid, h)){
+                right = mid - 1;
+                result = Math.min(result, mid);
+            }
+                
+            else left = mid + 1;
+        }
+
+        return result;
+    }
+
+    public boolean canEat(int[] piles, int k, int h){
+        int noOfHours = 0;
+        for(int pile : piles){
+            int quotient = pile/k;
+            int remainder = pile%k;
+            noOfHours+=quotient;
+            if(remainder!=0)
+                noOfHours+=1;
+        }
+
+        return noOfHours<=h;
+    }
+}
