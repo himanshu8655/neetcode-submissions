4ab7ff7c84d3e4059ba21bd103202979b1621b3d
himanshu8655 <himanshu8655@gmail.com>
2026-07-21 22:22:04 -0400

    Add: partition-equal-subset-sum - submission-2

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        int target = Arrays.stream(nums).max().getAsInt()/2;
+        if(n%2 == 1)
+            return false;
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(0);
+        for(int num : nums){
+            Set<Integer> set2 = new HashSet<>();
+            for(int a : set1){
+                set2.add(a);
+                set2.add(a + num);
+                if(a + num == target)
+                    return true;
+            }
+        }
+        return false;
+    }
+}
